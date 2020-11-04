ddc8f65e659885c32d8e5df90621232f27a3ba48
Hoaaa <meomincute98@gmail.com>
2020-11-04 09:13:57 +0700

    update client class

--- src/client/ChatClient3.java
+++ src/client/ChatClient3.java
@@ -9,3 +9,52 @@ import java.rmi.server.UnicastRemoteObject;
 import javax.swing.JOptionPane;
 
 import server.ChatServerIF;
+
+public class ChatClient3  extends UnicastRemoteObject implements ChatClient3IF {
+
+	private static final long serialVersionUID = 7468891722773409712L;
+	ClientRMIGUI chatGUI;
+	private String hostName = "localhost";
+	private String serviceName = "GroupChatService";
+	private String clientServiceName;
+	private String name;
+	protected ChatServerIF serverIF;
+	protected boolean connectionProblem = false;
+
+	public ChatClient3(ClientRMIGUI aChatGUI, String userName) throws RemoteException {
+		super();
+		this.chatGUI = aChatGUI;
+		this.name = userName;
+		this.clientServiceName = "ClientListenService_" + userName;
+	}
+
+	/**
+	 * Register our own listening service/interface
+	 * lookup the server RMI interface, then send our details
+	 * @throws RemoteException
+	 */
+	public void startClient() throws RemoteException {		
+		String[] details = {name, hostName, clientServiceName};	
+
+		try {
+			Naming.rebind("rmi://" + hostName + "/" + clientServiceName, this);
+			serverIF = ( ChatServerIF )Naming.lookup("rmi://" + hostName + "/" + serviceName);	
+		} 
+		catch (ConnectException  e) {
+			JOptionPane.showMessageDialog(
+					chatGUI.frame, "The server seems to be unavailable\nPlease try later",
+					"Connection problem", JOptionPane.ERROR_MESSAGE);
+			connectionProblem = true;
+			e.printStackTrace();
+		}
+		catch(NotBoundException | MalformedURLException me){
+			connectionProblem = true;
+			me.printStackTrace();
+		}
+		if(!connectionProblem){
+			registerWithServer(details);
+		}	
+		System.out.println("Client Listen RMI Server is running...\n");
+	}
+  
+}
