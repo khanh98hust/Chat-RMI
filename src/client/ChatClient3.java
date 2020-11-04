6cf89f933427ff90a1756a38c18f718ceec51dbe
Hoaaa <meomincute98@gmail.com>
2020-11-04 09:09:18 +0700

    create client

---
+++ src/client/ChatClient3.java
@@ -0,0 +1,11 @@
+package client;
+import java.net.MalformedURLException;
+import java.rmi.ConnectException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+import javax.swing.JOptionPane;
+
+import server.ChatServerIF;
