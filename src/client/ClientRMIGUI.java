c38e3a50475d889ca5082c4efab64766e1e49dd3
Hoang Mo <codaimohoang@gmail.com>
2020-11-17 23:12:44 +0700

    main method to start GUI app

--- src/client/ClientRMIGUI.java
+++ src/client/ClientRMIGUI.java
@@ -8,7 +8,6 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
-
 import javax.swing.BorderFactory;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -24,3 +23,41 @@ import javax.swing.ListSelectionModel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.border.Border;
+
+public class ClientRMIGUI extends JFrame implements ActionListener{
+
+	private static final long serialVersionUID = 1L;	
+	private JPanel textPanel, inputPanel;
+	private JTextField textField;
+	private String name, message;
+	private Font meiryoFont = new Font("Meiryo", Font.PLAIN, 14);
+	private Border blankBorder = BorderFactory.createEmptyBorder(10,10,20,10);//top,r,b,l
+	private ChatClient3 chatClient;
+  private JList<String> list;
+  private DefaultListModel<String> listModel;
+  
+  protected JTextArea textArea, userArea;
+  protected JFrame frame;
+  protected JButton privateMsgButton, startButton, sendButton;
+  protected JPanel clientPanel, userPanel;
+
+	/**
+	 * Main method to start client GUI app.
+	 * @param args
+	 */
+	public static void main(String args[]){
+		//set the look and feel to 'Nimbus'
+		try{
+			for(LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()){
+				if("Nimbus".equals(info.getName())){
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		}
+		catch(Exception e){
+			}
+		new ClientRMIGUI();
+		}//end main
+
+}
